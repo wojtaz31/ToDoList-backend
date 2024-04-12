2e9477337eedf46697b31aea4ffcfe2e10e7ac07
wojtaz31 <wojtaz31@wp.pl>
2024-04-12 13:03:51 +0200

    initial commit, created ToDoListItem class and instance of AspNetCore WebApplication

---
+++ backend.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+public class ToDoListItem
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public PriorityLevel Priority { get; set; }
+    public DateTime? DueDate { get; set; }
+    public string Status { get; set; }
+    public string[] Tags { get; set; }
+
+    public ToDoListItem(int id, string content, PriorityLevel priority = PriorityLevel.MEDIUM, DateTime? dueDate = null, string status = "Do zrobienia", string[] tags = null)
+    {
+        this.Id = id;
+        this.Content = content;
+        this.CreatedAt = DateTime.Now;
+        this.Priority = priority;
+        this.DueDate = dueDate;
+        this.Status = status;
+        this.Tags = tags;
+    }
+}
+
+public enum PriorityLevel
+{
+    [Description("HIGH")]
+    HIGH,
+    [Description("MEDIUM")]
+    MEDIUM,
+    [Description("LOW")]
+    LOW
+}
