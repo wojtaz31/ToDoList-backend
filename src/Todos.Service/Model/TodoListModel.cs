120031e6fbc94b3c7a512d92f371b5e4c28e6853
wojtaz31 <wojtaz31@wp.pl>
2024-04-25 13:47:32 +0200

    total API integration with SQL server database

---
+++ src/Todos.Service/Model/TodoListModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+public class TodoListModel
+{
+    public TodoListModel(ToDoList list)
+    {
+        Name = list.Name;
+        Description = list.Description;
+    }
+
+    [JsonConstructor]
+    public TodoListModel(){}
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
